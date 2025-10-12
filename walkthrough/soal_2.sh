d900e30cc0b6cffe1131dc411d48b3f58d3de68d
M. Hikari Reiziq Rakhadinta <reiziqzip@gmail.com>
2025-10-12 23:40:14 +0700

    update soal 2

--- walkthrough/soal_2.sh
+++ walkthrough/soal_2.sh
@@ -1,6 +1,9 @@
 # ==== EONWE ====
+echo "==== Check DNS EONWE ===="
 cat <<EOF > /root/.bashrc
 apt update
 apt install iptables -y
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -s 10.91.0.0/16
 EOF
+
+
